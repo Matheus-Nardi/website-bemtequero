ec69802cff831518563ea6e37b9b31e195d7faba
Matheus <matheuznardi@gmail.com>
2024-11-16 16:18:49 -0300

    feat: organização e mudança de contéudo

---
+++ src/contato.js
@@ -0,0 +1,8 @@
+
+const nome = document.getElementById('nome').value;
+const email = document.getElementById('email').value;
+const assunto = document.getElementById('assunto').value;
+const mensagem = document.getElementById('mensagem').value;
+
+
+
