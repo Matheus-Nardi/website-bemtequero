efd228d1b2af594cc6dc6322284dfe12e6902b6f
Matheus <matheuznardi@gmail.com>
2024-12-09 22:08:53 -0300

    ✨feat: responsivade, mudança de contéudo e atualizações gerais

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
-      
+      screens: {
+        sm: '480px', // Tamanho mínimo para telas pequenas
+        md: '768px', // Tamanho mínimo para telas médias
+        lg: '1024px', // Tamanho mínimo para telas grandes
+        xl: '1280px', // Tamanho mínimo para telas extragrandes
+      },
     },
   },
   plugins: [],
